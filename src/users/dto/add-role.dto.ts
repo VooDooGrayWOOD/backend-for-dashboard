9fd75a702544aef88e4902c0be7215134d12e30a
VooDoo <voodoograywood@gmail.com>
2025-07-31 13:51:12 +0700

    added validation exception & pipe (install class-transformer & class-validator)

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,4 +1,8 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class AddRoleDto {
+  @IsString({ message: 'It should be a string' })
   readonly value: string;
+  @IsNumber({}, { message: 'It should be a number' })
   readonly userId: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'user@gmail.com', description: 'User email address' })
+  @IsString({ message: 'It should be a string' })
+  @IsEmail({}, { message: 'Incorrect email' })
   readonly email: string;
 
   @ApiProperty({ example: '123', description: 'User password' })
+  @IsString({ message: 'It should be a string' })
+  @Length(4, 16, { message: 'No less than 4 and no more than 16 characters' })
   readonly password: string;
 }
