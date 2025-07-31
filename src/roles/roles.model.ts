a5df9bc2aa95e619d222c4324a16dd840225f542
VooDoo <voodoograywood@gmail.com>
2025-07-31 12:31:55 +0700

    add auth & roles modules

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../users/users.model';
+import { UserRoles } from './user-roles.model';
+
+interface RoleCreationAttrs {
+  value: string;
+  descrition: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Unique ID' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'admin', description: 'The value of the users role' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  value: string;
+
+  @ApiProperty({ example: 'Administrator', description: 'Role description' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
